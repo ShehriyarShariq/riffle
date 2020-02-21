e50caed15c70831abff744e2357248fbb8c0d7a2
Ludovic Barman <ludovic.barman@protonmail.com>
2020-02-21 15:40:40 +0100

    Changed files deterlab/run_all.sh

--- deterlab/run_all.sh
+++ deterlab/run_all.sh
@@ -5,14 +5,14 @@ source killall.sh
 client=5
 clientpermachine=1
 
-echo "Running on server 0"
+echo "Running on server 0  ./run_server.sh 0 ${client}"
 ssh dtrustee-0.lb-ldd-diss.safer.isi.deterlab.net "cd riffle/deterlab && ./run_server.sh 0 ${client}"
 echo "Running on server 1"
 ssh dtrustee-1.lb-ldd-diss.safer.isi.deterlab.net "cd riffle/deterlab && ./run_server.sh 1 ${client}"
 echo "Running on server 2"
 ssh dtrustee-2.lb-ldd-diss.safer.isi.deterlab.net "cd riffle/deterlab && ./run_server.sh 2 ${client}"
 
-echo "Running on client 0"
+echo "Running on client 0 ./run_client.sh 0 ${clientpermachine}"
 ssh dclient-0.lb-ldd-diss.safer.isi.deterlab.net "cd riffle/deterlab && ./run_client.sh 0 ${clientpermachine}"
 echo "Running on client 1"
 ssh dclient-1.lb-ldd-diss.safer.isi.deterlab.net "cd riffle/deterlab && ./run_client.sh 1 ${clientpermachine}"
