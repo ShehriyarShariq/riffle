3a9117c7309c9f563a8dc6290b19050cbbe6011e
Ludovic Barman <ludovic.barman@protonmail.com>
2020-02-21 15:55:01 +0100

    Changed files deterlab/run_client.sh

--- deterlab/run_client.sh
+++ deterlab/run_client.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 serverid=$1
 client=$2
 
-for id in $(seq 1 $client); do
+startId=$(($serverid * $client))
+end=$(($startId + $client -1))
+
+for id in $(seq $startId $end); do
   rm -f client${serverid}_${id}.log
   echo "Running ./client -i ${id} -s ./servers -m m > client${serverid}_${id}.log"
   nohup ./client -i ${id} -s ./servers -m m > client${serverid}_${id}.log 2>&1 &
