744bc9ac59edb0b219e1233ceba83410013aca8a
ShehriyarShariq <shehriyarshariq@gmail.com>
2024-08-06 11:48:30 +0200

    minimize rounds

--- lib/params.go
+++ lib/params.go
@@ -6,6 +6,6 @@ const BlockSize = 1024 //1KB for testing; 1MB for production
 //const BlockSize = 160
 const SecretSize = 256 / 8
 
-const MaxRounds = 10
+const MaxRounds = 2
 
 const ServerPort = 8000
